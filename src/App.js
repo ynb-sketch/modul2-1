a99e537998cff3f292e838c7f785a4cb8242b8b9
Elena Nekrasova <nek.elena155@gmail.com>
2025-01-19 19:47:33 +0300

    project js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,63 @@
 //В коде везде декларативный стиль, так как есть функции
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+// function App() {
+// 	const Year = new Date().getFullYear();
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 					<p>{Year}</p>
+// 				</a>
+// 			</header>
+// 		</div>
+// 	);
+// }
 
+//Используем только JavaScript
 function App() {
 	const Year = new Date().getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-					<p>{Year}</p>
-				</a>
-			</header>
-		</div>
+	return React.createElement(
+		"div",
+		{ className: "App" },
+		React.createElement(
+			"header",
+			{ className: "App-header" },
+			React.createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			React.createElement(
+				"p",
+				null,
+				"Edit ",
+				React.createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			React.createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React",
+				React.createElement("p", null, Year)
+			)
+		)
 	);
 }
 
